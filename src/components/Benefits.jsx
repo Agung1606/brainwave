0e6959794759525ecb7dc113b7f1009f4449f32f
Agung1606 <agngsptra928@gmail.com>
2024-05-20 19:52:57 +0700

    change the layout of benefits card

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -17,53 +17,55 @@ const Benefits = () => {
           title="Chat Smarter, Not Harder with Brainwave"
         />
 
-        <div className="flex flex-wrap gap-10 mb-10">
-          {benefits.map((item) => (
-            <div
-              key={item.id}
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
-              style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
-              }}
-            >
-              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
-                <h5 className="h5 mb-5">{item.title}</h5>
-                <p className="body-2 mb-6 text-n-3">{item.text}</p>
-                <div className="flex items-center mt-auto">
-                  <img
-                    src={item.iconUrl}
-                    width={48}
-                    height={48}
-                    alt={item.title}
-                  />
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
-                    Explore more
-                  </p>
-                  <Arrow />
-                </div>
-              </div>
-
-              {item.light && <GradientLight />}
+        <div className="flex items-center justify-center">
+          <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-10">
+            {benefits.map((item) => (
               <div
-                className="absolute inset-0.5 bg-n-8"
-                style={{ clipPath: `url(#benefits)` }}
+                key={item.id}
+                className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] max-w-[24rem]"
+                style={{
+                  backgroundImage: `url(${item.backgroundUrl})`,
+                }}
               >
-                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
-                  {item.imageUrl && (
+                <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                  <h5 className="h5 mb-5">{item.title}</h5>
+                  <p className="body-2 mb-6 text-n-3">{item.text}</p>
+                  <div className="flex items-center mt-auto">
                     <img
-                      src={item.imageUrl}
-                      width={380}
-                      height={362}
+                      src={item.iconUrl}
+                      width={48}
+                      height={48}
                       alt={item.title}
-                      className="w-full h-full object-cover"
                     />
-                  )}
+                    <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                      Explore more
+                    </p>
+                    <Arrow />
+                  </div>
                 </div>
-              </div>
 
-              <ClipPath />
-            </div>
-          ))}
+                {item.light && <GradientLight />}
+                <div
+                  className="absolute inset-0.5 bg-n-8"
+                  style={{ clipPath: `url(#benefits)` }}
+                >
+                  <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                    {item.imageUrl && (
+                      <img
+                        src={item.imageUrl}
+                        width={380}
+                        height={362}
+                        alt={item.title}
+                        className="w-full h-full object-cover"
+                      />
+                    )}
+                  </div>
+                </div>
+
+                <ClipPath />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </Section>
